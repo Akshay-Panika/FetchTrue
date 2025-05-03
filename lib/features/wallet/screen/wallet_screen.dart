53ad05f429368a636d4ae73d100dd73f9d94e6d9
akshe <akshey.panika@gmail.com>
2025-05-03 14:31:27 +0530

    Start project by Akshay at 03-05--25

--- lib/features/wallet/screen/wallet_screen.dart
+++ lib/features/wallet/screen/wallet_screen.dart
@@ -12,28 +12,54 @@ class WalletScreen extends StatelessWidget {
     WalletTransaction(id: 'f1a7895c', leadId: '100303', amount: 49.90, dateTime: DateTime(2025, 5, 2, 11, 42)),
     WalletTransaction(id: '727c66b1', leadId: '100301', amount: 2499.90, dateTime: DateTime(2025, 5, 2, 10, 18)),
     WalletTransaction(id: 'e721cb95', leadId: '100300', amount: 2499.90, dateTime: DateTime(2025, 4, 30, 18, 16)),
+    WalletTransaction(id: '8137be06', leadId: '100305', amount: 119.90, dateTime: DateTime(2025, 5, 2, 18, 21)),
+    WalletTransaction(id: '2d8a83dc', leadId: '100304', amount: 149.90, dateTime: DateTime(2025, 5, 2, 16, 14)),
+    WalletTransaction(id: 'f1a7895c', leadId: '100303', amount: 49.90, dateTime: DateTime(2025, 5, 2, 11, 42)),
+
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'My Wallet', showBackButton: true, showSearchIcon: true,showCartIcon: true,),
-      body: Column(
-        children: [
-          WalletBalanceCard(balance: balance),
-          Expanded(
-            child: Container(
-              color: Colors.grey[100],
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+            /// Wallet BalanceCard
+            WalletBalanceCard(balance: balance, totalEarning: 00,),
+            SizedBox(height: 20),
+            /// history
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                Text('Wallet History',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
+                Expanded(child: Divider()),
+        
+                SizedBox(width: 15,),
+                Row(
+                  spacing: 0,
+                  children: [
+                    Text('Filter',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+                    Icon(Icons.arrow_drop_down,size: 30,)
+                  ],
+                ),
+                ],
+              ),
+            ),
+            Expanded(
               child: ListView.builder(
-                padding: const EdgeInsets.all(12),
                 itemCount: transactions.length,
                 itemBuilder: (context, index) {
                   return WalletTransactionTile(transaction: transactions[index]);
                 },
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -41,8 +67,9 @@ class WalletScreen extends StatelessWidget {
 
 class WalletBalanceCard extends StatelessWidget {
   final double balance;
+  final double totalEarning;
 
-  const WalletBalanceCard({required this.balance});
+  const WalletBalanceCard({required this.balance, required this.totalEarning});
 
   @override
   Widget build(BuildContext context) {
@@ -59,23 +86,31 @@ class WalletBalanceCard extends StatelessWidget {
         ),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(24)),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(
         children: [
-          Text("Your Balance", style: TextStyle(color: Colors.black, fontSize: 16)),
-          const SizedBox(height: 10),
-          Text(
-            "₹ ${balance.toStringAsFixed(2)}",
-            style: TextStyle(color: Colors.black, fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildActionBtn(label: "Add", icon:Icons.add),
-              const SizedBox(width: 12),
-              _buildActionBtn(label: "Withdraw", icon:Icons.arrow_upward),
+              Text("Your Balance", style: TextStyle(color: Colors.black, fontSize: 16)),
+              const SizedBox(height: 10),
+              Text(
+                "₹ ${balance.toStringAsFixed(2)}",
+                style: TextStyle(color: Colors.green, fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  _buildActionBtn(label: "Add", icon:Icons.add),
+                  const SizedBox(width: 12),
+                  _buildActionBtn(label: "Withdraw", icon:Icons.arrow_upward),
+                ],
+              ),
             ],
           ),
+          
+          Align(
+              alignment: Alignment.topRight,
+              child: Text('Total Earning ₹ $totalEarning', style: TextStyle(fontSize: 12,fontWeight: FontWeight.w600, color: Colors.blueAccent),))
         ],
       ),
     );
@@ -87,8 +122,8 @@ class WalletBalanceCard extends StatelessWidget {
       child: Row(
         spacing: 5,
         children: [
-           Icon(icon, size: 18),
-          Text(label!, style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14)),
+           Icon(icon, size: 18, color: Colors.white,),
+          Text(label!, style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14, color: Colors.white)),
         ],
       ),
     );
@@ -105,14 +140,13 @@ class WalletTransactionTile extends StatelessWidget {
     final formattedDate = DateFormat('d MMM, y hh:mm a').format(transaction.dateTime);
 
     return CustomContainer(
-      margin: const EdgeInsets.symmetric(vertical: 8),
       child: Row(
         spacing: 5,
         children: [
           CustomContainer(
             margin: EdgeInsets.all(5),
-            padding: EdgeInsets.all(5),
-            child: Icon(Icons.attach_money, color: Colors.blueAccent),
+            height: 50,width: 50,
+            child: Center(child: Text('₹', style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),))
           ),
           Expanded(
             child: Column(
@@ -133,8 +167,8 @@ class WalletTransactionTile extends StatelessWidget {
             children: [
               SizedBox(),
               Text(
-                "+ ₹${transaction.amount.toStringAsFixed(2)}",
-                style: TextStyle(color: Colors.green[700], fontWeight: FontWeight.bold, fontSize: 16),
+                "+ ₹ ${transaction.amount.toStringAsFixed(2)}",
+                style: TextStyle(color: Colors.green[700], fontWeight: FontWeight.bold, fontSize: 14),
               ),
             ],
           ),
